af806a9b702d1b6d9004485cd757811ed1ea8473
Jasper <zhanhanjie123@163.com>
2021-12-07 09:24:52 +0800

    添加重定向配置类

---
+++ src/main/java/edu/hzu/englishstudyweb/config/RedirectConfig.java
@@ -0,0 +1,18 @@
+package edu.hzu.englishstudyweb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RedirectConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/").setViewName("study");
+        registry.addViewController("/study").setViewName("study");
+        registry.addViewController("/register").setViewName("register");
+        registry.addViewController("/error").setViewName("error");
+    }
+}
