e3708b1dd0423e741243285fadb0f5cfe972ac68
Jasper <zhanhanjie123@163.com>
2021-12-08 15:29:39 +0800

    修改学习页面

--- src/main/resources/static/js/study.js
+++ src/main/resources/static/js/study.js
@@ -1,3 +1,24 @@
+$('#collection').bind('click', function () {
+    $.ajax({
+        url: 'collection/add/',
+        type: 'post',
+        dataType: 'text',
+        data: {
+            wordId: $('#word_id').text()
+        },
+        cache: false,
+        async: true,
+        success: function (data) {
+            if (data == "已收藏") {
+                $("#collection").addClass("disabled");
+                $("#collection").empty();
+                $("#collection").append("已收藏");
+            } else {
+                alert("收藏失败");
+            }
+        }
+    })
+})
 $('#btn_know').bind('click', function () {
     $.ajax({
         url: '/study/know',
@@ -64,4 +85,31 @@ function modifyData(data) {
     $("#word_english").append(jsonObject.word_english);
     $("#word_total").empty();
     $("#word_total").append("剩余数量："+jsonObject.word_total);
+    $("#word_id").empty();
+    $("#word_id").append(jsonObject.word_id);
+    if (jsonObject.word_collect == "已收藏") {
+        $("#collection").addClass("disabled");
+    } else {
+        $("#collection").removeClass("disabled");
+    }
+    $("#collection").empty();
+    $("#collection").append(jsonObject.word_collect);
+
 }
+// if (jsonObject.word_collect == "已收藏") {
+//     if (!$("#collection").hasClass("disabled")) {
+//         $("#collection").removeClass("disabled");
+//         $("#collection").addClass("disabled");
+//         $("#collection").empty();
+//         $("#collection").append(jsonObject.word_collect);
+//     }
+// } else {
+//     if ($("#collection").hasClass("disabled")) {
+//         $("#collection").removeClass("disabled");
+//         $("#collection").empty();
+//         $("#collection").append(jsonObject.word_collect);
+//     }
+// }
+//
+//
+// $("#collection").attr("href", "/collection/add/"+jsonObject.word_id);
